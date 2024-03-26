bf09d6b5789985e45e5f68e716df2d6b6668fe03
vineetsemwal <vineetsemwal82@gmail.com>
2024-03-26 15:55:15 +0530

    var , const, let demo

---
+++ javascript/badvar.js
@@ -0,0 +1,15 @@
+
+function printEven(num){
+
+    
+    if(num%2==0){
+        var msg="number is even";
+        console.log(msg);
+    }
+
+    console.log("message is ", msg);
+
+}
+
+printEven(10);
+
---
+++ javascript/const_let.js
@@ -0,0 +1,26 @@
+
+
+function printEven(num){
+    
+    if(num%2==0){
+        const msg="number is even";
+        console.log(msg);
+    }
+
+  //  console.log("message is ", msg);
+
+}
+
+printEven(10);
+
+
+function printMessage(){
+    {
+        let msg="hello"
+        msg="welcome";
+        console.log("message is ",msg);
+    }
+    
+}
+
+printMessage();
